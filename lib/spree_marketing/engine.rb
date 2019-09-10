73d51adef0b0a0a9f6e2260c091b30eff3128873
dylanfisher <hi@dylanfisher.com>
2019-09-09 21:30:34 -0400

    Don't manually load models and decorators
    From some very quick testing, this doesn't seem to be necessary. This
    code had the unfortunate side effect of re-loading already defined
    classes, causing superclass mismatches, etc.

--- lib/spree_marketing/engine.rb
+++ lib/spree_marketing/engine.rb
@@ -30,16 +30,16 @@ module SpreeMarketing
       ]
     end
 
-    def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
-        Rails.configuration.cache_classes ? require(c) : load(c)
-      end
+    # def self.activate
+    #   Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
+    #     Rails.configuration.cache_classes ? require(c) : load(c)
+    #   end
 
-      Dir.glob(File.join(File.dirname(__FILE__), '../../app/models/**/*.rb')) do |c|
-        Rails.configuration.cache_classes ? require(c) : load(c)
-      end
-    end
+    #   Dir.glob(File.join(File.dirname(__FILE__), '../../app/models/**/*.rb')) do |c|
+    #     Rails.configuration.cache_classes ? require(c) : load(c)
+    #   end
+    # end
 
-    config.to_prepare &method(:activate).to_proc
+    # config.to_prepare &method(:activate).to_proc
   end
 end
